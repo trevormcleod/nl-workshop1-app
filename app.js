964fdc0f800d324c4fa45af1f3188e42b6b00c4c
Trevor McLeod <tamcleod1@gmail.com>
2012-12-17 15:51:00 -0500

    first instagram call

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 
 var express = require('express')
   , routes = require('./routes')
+  , instagram = require('./routes/instagram')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
@@ -27,9 +28,18 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+
+//Instagram Realtime API integration
+
+app.get('/popular', instagram.popular);
+
+
+
 app.get('/', routes.index);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+
