5eca3a5d90430ca1aaa04179b61773dc325e2b74
Tim Atkinson <timisbusy@gmail.com>
2013-01-10 17:37:08 -0500

    reworking routes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,18 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
+// exports.index = function(req, res){
+//   res.render('index', { title: 'Express' });
+// };
+
+module.exports.create = function (app) {
+  app.get('/', function (req, res) {
+    res.render('index', {
+      title: "home"
+    });
+  });
+
+  app.get('/explore', function (req, res) {
+    res.render('explore');
+  });
+}
