1723e9a460d5f1a411d8a7248bf0d421ee862933
falk <falk@logicparty.org>
2013-01-30 17:30:17 -0500

    add session/cookie middleware

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.configure(function(){
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({secret: 'Node Rocks!'}));
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
