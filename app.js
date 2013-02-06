bdd50cca40d8490dcb30401de23048c9bf944991
falk <falk@logicparty.org>
2013-02-05 22:33:28 -0500

    add socket.io

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ var express = require('express')
   , http = require('http')
   , path = require('path');
 
-var app = express();
+var app = express()
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server)
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -34,6 +36,6 @@ app.configure('development', function(){
 
 routes.create(app);
 
-http.createServer(app).listen(app.get('port'), function(){
+server.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
