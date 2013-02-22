cfd92102c8b510e1c758b23b6fd0b55dce18386d
falk <falk@logicparty.org>
2013-02-22 12:21:01 -0500

    update client secret and id

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ module.exports = {
   host: "http://nodelingo.sampleapp.jit.su",
   mongo_uri: "mongodb://nodelingo:GAworkshop@linus.mongohq.com:10099/instadb",
   instagram: {
-    client_id: '048746d02c444198b88697aa3920b5b4',
-    client_secret: '0a32a7b0349a4d33b16c4bbb2dbf3fec'
+    client_id: '2437b9283cfe4b79a430c7a9a97c76b0',
+    client_secret: 'e795d662eec8455985242a609c6b271f'
   }
 }
